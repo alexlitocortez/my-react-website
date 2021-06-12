4f581d8d211cedb15bdbde405e041919aa706549
Alex Cortez <acim650@gmail.com>
2021-06-12 15:12:23 -0700

    src/globalStyles.js

---
+++ src/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+`;
+
+
+export default GlobalStyle;
