946ab093f29563ec9944bbb8d9e1884df61c25b9
Alex Cortez <acim650@gmail.com>
2021-08-03 11:23:28 -0700

    src/components

---
+++ src/components/HeroSection/BackgroundColor.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import styled from 'styled-components';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    section: {
+        height: '400px',
+        width: '100%',
+        margin: '0',
+        padding: '1rem 0 0',
+        textAlign: 'center',
+        top: '100px',
+        left: 0,
+        right: 0,
+        backgroundColor: 'black',
+        color: 'white',
+        position: 'relative',
+        bottom: '200px',
+    },
+    header: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '2rem'
+    }
+}))
+
+function BackgroundColor() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.section}>
+            <h1 className={classes.header}>Hi, I’m Alex. Nice to meet you.</h1>
+            <div>
+            Since beginning my journey as a freelance designer nearly 10 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. 
+            I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+            </div>
+        </div>
+    )
+}
+
+export default BackgroundColor
+
+
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components';
 import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import { BsCodeSlash } from 'react-icons/bs';
+import BackgroundColor from '/Users/l/my-react-website/src/components/HeroSection/BackgroundColor.js';
+import './HeroSection.css'
+
+const CodingImage = styled(BsCodeSlash)`
+    display: block;
+    background-image: url(${BsCodeSlash});
+    height: 50px;
+    width: 40px;
+`;
 
 const useStyles = makeStyles((theme) => ({
     hero: {
@@ -20,34 +31,40 @@ const useStyles = makeStyles((theme) => ({
     card: {
         width: '400px',
         height: '600px',
+        position: 'relative',
+        bottom: '15rem',
+        fontSize: '12px',
         padding: '70px',
         boxShadow: '0 3px 5px 2px rgba(0, 0, 0, 0.2)',
         '@media (max-width: 768px)': {
-            width: '410px'
+            width: '410px',
     },
-    backgroundColor: {
-        height: '300px',
-        width: '300px'
+    centerImage: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative'
     }
     }
 }))
-
 
 function HeroSection() {
     const classes = useStyles();
 
     return (
-        <div className={classes.hero}>
-            <div className={classes.backgroundColor}>
-                Hi
-            </div>
-                <Card className={classes.card}>
-                    Languages I know:
+        <div>
+            <BackgroundColor />
+            <div className={classes.hero}>
+                <Card className={classes.card} style={{ zIndex: 1000}}>
+                    <div className={classes.centerImage}>
+                        <CodingImage />
+                    </div>
+                        <h1 className='header'>Languages</h1>
                     <li>HTML</li>
                     <li>CSS</li>
                     <li>Javascript</li>
                     <li>React</li>
                 </Card>
+            </div>
         </div>
     )
 }
